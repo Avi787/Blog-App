46d51cd45874bc8a64b14e89e291dbe0a7060d03
Avi Tasks <workavi78@gmail.com>
2019-04-07 00:59:34 +0200

    Add flash.now to article create failure path

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -26,7 +26,6 @@ RSpec.feature "Creating Articles" do
     click_button "Create Article"
     
     expect(page).to have_content("Article has not been created")
-    #expect(page.current_path).to eq(new_article_path)
     expect(page).to have_content("Title can't be blank")
     expect(page).to have_content("Body can't be blank")
   
