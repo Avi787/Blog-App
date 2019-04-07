0c41063b4dce18555e03926537c2d3da8a3ab2c2
Avi Tasks <workavi78@gmail.com>
2019-04-07 03:33:28 +0200

    Implemeted showing article details

---
+++ spec/features/show_article_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.feature "Showing an article" do
+  
+  before do
+    @article = Article.create(title: "The first article", body: "Lorem ipsum dolor sit omet, consectetur")
+  end
+  
+  scenario "A user shows an article" do
+    visit "/"
+    click_link @article.title
+    
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(page.current_path).to eq(article_path(@article))
+    
+    
+    
+  end
+end
+  
+  
+  
